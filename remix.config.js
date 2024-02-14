3c0852322f2e670b5f2c32ca03e189a7a8378572
Vincent <vialin1128@gmail.com>
2024-02-13 22:08:53 -0800

    add nextui

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
+module.exports = {
+  appDirectory: 'app',
+  browserBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'build',
+  devServerPort: 8002,
+  serverModuleFormat: 'cjs',
 };
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+import {nextui} from "@nextui-org/react";
+
+export default {
+  content: ['./app/**/*.{js,jsx,ts,tsx}',"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode:'class',
+  plugins: [nextui()],
+} satisfies Config
+
